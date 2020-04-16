a2a9bcb711096b31f5975700106e434bafb87454
janeth <jarri023@fiu.edu>
2020-04-16 09:55:18 -0400

    professional projects logic for viewing info on each project

--- src/components/ProfessionalProjects.js
+++ src/components/ProfessionalProjects.js
@@ -1,18 +1,52 @@
-import React from "react";
-import wisecode from "../images/wise-code.png";
-import wisedb from "../images/wisedb.png";
+import React, { Component } from "react";
+import WiseCode from "./projects/WiseCode";
+import wiseCode from "../images/wise-code.png";
+import wiseDB from "../images/wisedb.png";
+import Wisedb from "./projects/Wisedb";
 
-const ProfessionalProjects = () => {
-  return (
-    <div className="all-projects">
-      <div className="project-box">
-        <img src={wisecode} alt="wise-code mobile app" />
+class ProfessionalProjects extends Component {
+  constructor() {
+    super();
+    this.state = {
+      wisecode: false,
+      wisedb: false,
+    };
+  }
+
+  selectProject = (project) => {
+    if (this.state[project]) {
+      this.setState({ [project]: false });
+    } else {
+      this.setState({ [project]: true });
+    }
+  };
+
+  render() {
+    const { wisecode, wisedb } = this.state;
+    return (
+      <div className="all-projects">
+        <div className={wisecode || wisedb ? "overlay top" : "overlay"}></div>
+
+        <div
+          className={wisecode ? "project-box select" : "project-box"}
+          onClick={() => this.selectProject("wisecode")}
+        >
+          <img src={wiseCode} alt="wise-code mobile app" />
+        </div>
+        <WiseCode display={wisecode} />
+
+        <div
+          className={
+            wisedb ? "project-box bottom select" : "project-box bottom"
+          }
+          onClick={() => this.selectProject("wisedb")}
+        >
+          <img src={wiseDB} alt="wisedb app" />
+        </div>
+        <Wisedb display={wisedb} />
       </div>
-      <div className="project-box bottom">
-        <img src={wisedb} alt="wisedb app" />
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ProfessionalProjects;
