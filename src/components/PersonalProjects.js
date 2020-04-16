f9bcdec8ce0907d94c18ff6cdd9f7df1afec02f2
janeth <jarri023@fiu.edu>
2020-04-16 09:54:03 -0400

    added overlay for info view in personal project page

--- src/components/PersonalProjects.js
+++ src/components/PersonalProjects.js
@@ -1,21 +1,34 @@
-import React from "react";
-import geektext from "../images/geektext.png";
-import graphtheory from "../images/graphtheory.png";
-import rhaz from "../images/rhaz.png";
-const PersonalProjects = () => {
-  return (
-    <div className="all-projects">
-      <div className="project-box">
-        <img src={geektext} alt="geek text web app" />
+import React, { Component } from "react";
+import geekText from "../images/geektext.png";
+import graphTheory from "../images/graphtheory.png";
+import RHAZ from "../images/rhaz.png";
+class PersonalProjects extends Component {
+  constructor() {
+    super();
+    this.state = {
+      geektext: false,
+      graphtheory: false,
+      rhaz: false
+    }
+  }
+  render() {
+    const {geektext, graphtheory, rhaz} = this.state;
+    return (
+      <div className="all-projects">
+        <div className={geektext || graphtheory || rhaz ? "overlay top" : "overlay"}></div>
+
+        <div className="project-box">
+          <img src={geekText} alt="geek text web app" />
+        </div>
+        <div className="project-box bottom">
+          <img src={graphTheory} alt="graph theory web app" />
+        </div>
+        <div className="project-box">
+          <img src={RHAZ} alt="rhaz game app" />
+        </div>
       </div>
-      <div className="project-box bottom">
-        <img src={graphtheory} alt="graph theory web app" />
-      </div>
-      <div className="project-box">
-        <img src={rhaz} alt="rhaz game app" />
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default PersonalProjects;
