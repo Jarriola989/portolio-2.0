ba89d7a7cb237b069eb145b4937836f8946d6db0
janeth <jarri023@fiu.edu>
2020-04-17 02:44:13 -0400

    added skills and contact components to router

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import { BrowserRouter, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <BrowserRouter>
       <Route path="/" component={LandingPage} />
       <Route path="/" component={Navbar} />
+      <Route path="/" component={Skills} />
       <Route path="/" component={Projects} />
-      {/* <Navbar />
-      <LandingPage />
-      <Projects /> */}
+      <Route path="/" component={Contact} />
     </BrowserRouter>
   );
 }
