752ec039bdd98d2c60b6cfc4e96bf73a0291d06c
janeth <jarri023@fiu.edu>
2020-04-23 01:18:19 -0400

    resume button opens pdf

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
+import Resume from "./components/Resume";
 
 function App() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import resume from "../documents/resume.pdf";
 import "./Navbar.css";
 
 class Navbar extends Component {
   render() {
     return (
       <div className="navbar">
-        <Link to="/aboutme">Resume</Link>
+        <a href={resume} target="_blank" rel="noopener noreferrer">
+          Resume
+        </a>
         <Link to="/">Skills</Link>
         <Link to="/#projects">Projects</Link>
         <Link to="/">Contact</Link>
