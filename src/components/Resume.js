8795128522a8e1f556835d0fa1e09e0389e6242b
janeth <jarri023@fiu.edu>
2020-04-23 01:18:34 -0400

    pdf viewer to finish later

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
-import { Document } from "react-pdf";
+import { Document, Page } from "react-pdf";
+// import resume from "./resume.pdf";
 
+//TODO: Update React and pdf viewer
 class Resume extends Component {
+  constructor() {
+    super();
+    this.state = {
+      numPages: null,
+    };
+  }
+
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  };
+
   render() {
     return (
       <div>
-        <Document file="./documents/JanethArriola_Resume.pdf"></Document>
+        <Document
+          file="./resume.pdf"
+          onLoadSuccess={this.onDocumentLoadSuccess}
+          onLoadError={console.error}
+        >
+          <Page pageNumber={1} width={500} />
+        </Document>
+        {console.log(this.state.file)}
+        {/* <a href={resume}></a> */}
       </div>
     );
   }
