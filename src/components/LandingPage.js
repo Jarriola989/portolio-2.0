015101334f21bd61bb6dc5b55137eb2fa8841e08
janeth <jarri023@fiu.edu>
2020-04-24 01:51:21 -0400

    added mobile menu button

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import "./LandingPage.css";
+import "./MobileMenu.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMinus } from "@fortawesome/free-solid-svg-icons";
 
 class LandingPage extends Component {
   constructor() {
@@ -12,7 +15,19 @@ class LandingPage extends Component {
         <div className="block one">Janeth</div>
         <div className="block two">Arriola's</div>
         <div className="block three">Portfolio</div>
-        <div className="nav-block"></div>
+        <button className="mobile-menu">
+          <div className="mobile-menu-1">
+            <FontAwesomeIcon icon={faMinus} className="first-bar" />
+            <FontAwesomeIcon icon={faMinus} className="second-bar"/>
+          </div>
+          <div className="mobile-menu-2">
+            <FontAwesomeIcon icon={faMinus} className="first-bar" />
+            <FontAwesomeIcon icon={faMinus} className="second-bar" />
+          </div>
+          <div className="mobile-menu-3">
+            <FontAwesomeIcon icon={faMinus} />
+          </div>
+        </button>
       </div>
     );
   }
