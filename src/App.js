9e90553eb1387e8eb6b7298b45502f94f7a6deb7
janeth <jarri023@fiu.edu>
2020-04-25 21:50:24 -0400

    removed browser router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
-import Resume from "./components/Resume";
+import MobileMenu from "./components/MobileMenu";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Route path="/" component={LandingPage} />
-      <Route path="/" component={Navbar} />
-      <Route path="/" component={Skills} />
-      <Route path="/" component={Projects} />
-      <Route path="/" component={Contact} />
-    </BrowserRouter>
+    <div>
+      <LandingPage />
+      <Navbar />
+      <MobileMenu />
+      <Skills />
+      <Projects />
+      <Contact />
+    </div>
   );
 }
 
