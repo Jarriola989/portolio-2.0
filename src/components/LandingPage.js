506effd45f29e961576ea1cde5040737faca65be
janeth <jarri023@fiu.edu>
2020-04-25 21:50:43 -0400

    mobile menu panel

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,33 +1,28 @@
 import React, { Component } from "react";
 import "./LandingPage.css";
-import "./MobileMenu.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMinus } from "@fortawesome/free-solid-svg-icons";
 
 class LandingPage extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      mobilemenu: false,
+    };
   }
+
+  toggleMobileMenu = () => {
+    if (this.state.mobilemenu === true) {
+      this.setState({ mobilemenu: false });
+    } else {
+      this.setState({ mobilemenu: true });
+    }
+  };
+
   render() {
     return (
       <div className="landing">
         <div className="block one">Janeth</div>
         <div className="block two">Arriola's</div>
         <div className="block three">Portfolio</div>
-        <button className="mobile-menu">
-          <div className="mobile-menu-1">
-            <FontAwesomeIcon icon={faMinus} className="first-bar" />
-            <FontAwesomeIcon icon={faMinus} className="second-bar"/>
-          </div>
-          <div className="mobile-menu-2">
-            <FontAwesomeIcon icon={faMinus} className="first-bar" />
-            <FontAwesomeIcon icon={faMinus} className="second-bar" />
-          </div>
-          <div className="mobile-menu-3">
-            <FontAwesomeIcon icon={faMinus} />
-          </div>
-        </button>
       </div>
     );
   }
---
+++ src/components/MobileMenu.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import resume from "../documents/resume.pdf";
+import "./MobileMenu.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes, faMinus } from "@fortawesome/free-solid-svg-icons";
+
+class MobileMenu extends Component {
+  constructor() {
+    super();
+    this.state = {
+      open: false,
+    };
+  }
+
+  openMenu = () => {
+    this.setState({
+      open: true,
+    });
+  };
+
+  closeMenu = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button className="mobile-menu" onClick={this.openMenu}>
+          <div className="mobile-menu-1">
+            <FontAwesomeIcon icon={faMinus} className="first-bar" />
+            <FontAwesomeIcon icon={faMinus} className="second-bar" />
+          </div>
+          <div className="mobile-menu-2">
+            <FontAwesomeIcon icon={faMinus} className="first-bar" />
+            <FontAwesomeIcon icon={faMinus} className="second-bar" />
+          </div>
+          <div className="mobile-menu-3">
+            <FontAwesomeIcon icon={faMinus} />
+          </div>
+        </button>
+        <div className={this.state.open ? "side-panel" : "side-panel hidden"}>
+          <FontAwesomeIcon
+            icon={faTimes}
+            className={
+              this.state.open ? "exit-mobile-menu" : "exit-mobile-menu hidden"
+            }
+            onClick={this.closeMenu}
+          />
+          <a href={resume} target="_blank" rel="noopener noreferrer">
+            Resume
+          </a>
+          <a href="#skills">Skills</a>
+          <a href="#projects">Projects</a>
+          <a href="#contact">Contact</a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MobileMenu;
