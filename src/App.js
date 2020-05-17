c0c6363e20dfbcfc7d1ab3d8d1479293aad7d1e6
janeth <jarri023@fiu.edu>
2020-05-17 15:10:36 -0400

    footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import MobileMenu from "./components/MobileMenu";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,55 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import "./Footer.css";
+import repl from "../images/repl-icon.png";
+import trello from "../images/trello-icon.png";
+import linkedin from "../images/linkedin-icon.png";
 
 const Footer = () => {
   return (
     <footer>
-      <a
-        href="https://www.linkedin.com/in/janeth-arriola-a01463158/"
-        rel="noopener noreferrer"
-        target="_blank"
-        className="linkedin"
-      >
-        <FontAwesomeIcon icon={faLinkedin} className="linkedin" />
-      </a>
+      <div className="footer-name">
+        <h1>Janeth Arriola</h1>
+        <h3>Software Developer</h3>
+      </div>
 
-      <a
-        href="https://github.com/Jarriola989"
-        rel="noopener noreferrer"
-        target="_blank"
-        className="github"
-      >
-        <FontAwesomeIcon icon={faGithub} />
-      </a>
+      <div className="copyright">
+        This portfolio was built and designed by Janeth Arriola using React,
+        Javascript, HTML5, and CSS3.
+      </div>
+      <div className="footer-links">
+        <a
+          href="https://www.linkedin.com/in/janeth-arriola-a01463158/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img src={linkedin} alt="linkedin icon" className="icon-img" />
+          {/* <FontAwesomeIcon icon={faLinkedin} className="linkedin" /> */}
+        </a>
+
+        <a
+          href="https://github.com/Jarriola989"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a
+          href="https://repl.it/@Jarriola989"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img src={repl} alt="repl icon" className="icon-img" />
+        </a>
+        <a
+          href="https://trello.com/janetharriola/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img src={trello} alt="trello icon" className="icon-img" />
+        </a>
+      </div>
     </footer>
   );
 };
